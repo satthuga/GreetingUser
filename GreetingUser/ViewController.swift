fc229cafecf8d43e3745e3acf2a29f52e171ccfc
Apple <apple@MacBook-Pro.local>
2021-06-07 15:30:23 +0700

    Create Greeting User

---
+++ GreetingUser/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  GreetingUser
+//
+//  Created by Apple on 07/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var greetingUser: UILabel!
+    @IBOutlet weak var shoppingButton: UIButton!
+    
+    var userName = "Linda"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        greetingUser.text = "Hello " + userName + "!"
+        
+        shoppingButton.layer.cornerRadius = 30
+        
+        userImage.image = UIImage(named: "13")
+        
+        userImage.layer.cornerRadius = userImage.frame.size.width / 2
+        userImage.contentMode = .scaleAspectFill
+        
+        userImage.layer.borderWidth = 10
+        userImage.layer.borderColor = UIColor.gray.cgColor
+    }
+
+
+}
+
